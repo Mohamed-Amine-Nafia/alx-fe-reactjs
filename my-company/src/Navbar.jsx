7190390cbf1846004ad6df32eb1a662c9aa80816
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-07 00:40:41 +0200

    add my-company project

---
+++ my-company/src/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  const linkStyle = {
+    margin: "0 10px",
+    textDecoration: "none",
+    color: "white",
+  };
+  const navStyle = { padding: "10px", backgroundColor: "#333" };
+
+  return (
+    <nav style={navStyle}>
+      <Link to="/" style={linkStyle}>
+        Home
+      </Link>
+      <Link to="/about" style={linkStyle}>
+        About
+      </Link>
+      <Link to="/services" style={linkStyle}>
+        Services
+      </Link>
+      <Link to="/contact" style={linkStyle}>
+        Contact
+      </Link>
+    </nav>
+  );
+}
