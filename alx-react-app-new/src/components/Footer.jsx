9769f776f7213ef8318fe3972e43923f87204815
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-06 22:55:40 +0200

    duplicate the prev directory and apply some styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ background: "teal", padding: "2rem 0" }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main style={{ background: "tomato", padding: "2rem 0" }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
