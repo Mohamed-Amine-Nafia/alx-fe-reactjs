f432ccff7a45affd910c3cb2a32f93c23ecfb065
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-12 21:50:13 +0200

    fix the code

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,6 @@
 import { useRecipeStore } from "./recipeStore.js";
 
-export const RecipeList = () => {
+const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
@@ -14,3 +14,4 @@ export const RecipeList = () => {
     </div>
   );
 };
+export default RecipeList;
