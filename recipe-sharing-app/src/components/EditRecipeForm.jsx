51674ab1ece14c5a5729212eaf1e34ad4e60fc01
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-12 22:53:13 +0200

    commit

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -6,8 +6,8 @@ const EditRecipeForm = ({ recipe }) => {
   const [title, setTitle] = useState(recipe.title);
   const [description, setDescription] = useState(recipe.description);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
     updateRecipe({ ...recipe, title, description });
   };
 
@@ -27,4 +27,5 @@ const EditRecipeForm = ({ recipe }) => {
     </form>
   );
 };
+
 export default EditRecipeForm;
