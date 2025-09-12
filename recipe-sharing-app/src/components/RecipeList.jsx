12af30bfe872a120e0df7cc493130996e14e0354
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-12 17:07:30 +0200

    create store folder for zustand and add recipeStore.js

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import { useRecipeStore } from "../RecipeStore";
+import { useRecipeStore } from "../store/RecipeStore";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
