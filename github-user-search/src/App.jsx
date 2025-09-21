f00ca5f5b8963fd7ab9283678fcfeadce9546e2c
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-21 13:39:07 +0200

    add github-user-search directory

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search App</h1>
+      <p>Project setup successful 🚀</p>
+    </div>
+  );
+}
+
+export default App;
