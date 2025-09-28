9ab5420123d3e468776dc6efd7420606cb8ce9ef
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-29 00:32:53 +0200

    make teh layout responsive

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 max-w-sm mx-auto my-20 p-8 rounded-lg shadow-lg">
+    <div className="bg-gray-100 max-w-sm max-w-xs mx-auto my-20 sm:p-4 md:p-8 rounded-lg shadow-lg">
       <img
-        className="w-36 h-36 rounded-full mx-auto"
+        className="md:w-36 md:h-36 sm:w-24 sm:h-24 rounded-full mx-auto"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
