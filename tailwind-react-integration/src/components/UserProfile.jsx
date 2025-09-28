e786f3e8a64d49a50ae3c1d01dcf13762c23ba68
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-29 00:37:07 +0200

    Adding Advanced Interactivity and Transitions with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 md:max-w-sm sm:max-w-xs mx-auto my-20 sm:p-4 md:p-8 rounded-lg shadow-lg">
+    <div className="hover:shadow-xl bg-gray-100 md:max-w-sm sm:max-w-xs mx-auto my-20 sm:p-4 md:p-8 rounded-lg shadow-lg">
       <img
-        className="md:w-36 md:h-36 sm:w-24 sm:h-24 rounded-full mx-auto"
+        className="md:w-36 md:h-36 sm:w-24 sm:h-24 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4 hover:text-blue-500">
+        John Doe
+      </h1>
       <p className="text-gray-600 sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
