6f248867c863ff7d1de9e6f7af5f566648d7e357
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-29 00:34:13 +0200

    fix the layout

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 max-w-sm max-w-xs mx-auto my-20 sm:p-4 md:p-8 rounded-lg shadow-lg">
+    <div className="bg-gray-100 md:max-w-sm sm:max-w-xs mx-auto my-20 sm:p-4 md:p-8 rounded-lg shadow-lg">
       <img
         className="md:w-36 md:h-36 sm:w-24 sm:h-24 rounded-full mx-auto"
         src="https://via.placeholder.com/150"
