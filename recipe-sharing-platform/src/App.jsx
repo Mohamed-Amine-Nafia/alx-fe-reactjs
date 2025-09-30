666da88a8a5d25cbf3e607f154258578dc5e72f1
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-30 21:58:51 +0200

    create recipe sharing platform directory

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="bg-red-400 text-center text-5xl p-12 font-extrabold">
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
